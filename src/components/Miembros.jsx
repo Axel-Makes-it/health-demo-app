fddcd464b3b32c028107b6841cffd8e0441ec06f
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-07 20:19:53 -0500

    update

--- src/components/Miembros.jsx
+++ src/components/Miembros.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../styles/Miembros.css";
+import searchIcon from "../images/magnify.svg";
 
 function Miembros() {
   return (
     <section className="miembros__container">
       <h2>Lista de miembros</h2>
+      <div className="miembros__form">
+        <form>
+          <img src={searchIcon} alt="search" width={24} />
+          <input
+            type="text"
+            placeholder="Buscar por nombre y apellido o CardID"
+          />
+        </form>
+      </div>
     </section>
   );
 }
