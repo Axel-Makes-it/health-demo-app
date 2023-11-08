81baf21bde0c374f96b169a3cb22bb9dfc3f4201
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-07 19:17:48 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SideBar from "./components/SideBar";
 import "./App.css";
+import Miembros from "./components/Miembros";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <Miembros />
     </div>
   );
 }
---
+++ src/components/Miembros.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Miembros.css";
+
+function Miembros() {
+  return <section className="miembros__container">Miembros</section>;
+}
+
+export default Miembros;
