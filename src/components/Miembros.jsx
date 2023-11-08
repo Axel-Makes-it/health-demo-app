7f1d740987ab2baec6ae1235b04fc77f0b3aa6d3
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-07 19:53:39 -0500

    update

--- src/components/Miembros.jsx
+++ src/components/Miembros.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/Miembros.css";
 
 function Miembros() {
-  return <section className="miembros__container">Miembros</section>;
+  return (
+    <section className="miembros__container">
+      <h2>Lista de miembros</h2>
+    </section>
+  );
 }
 
 export default Miembros;
